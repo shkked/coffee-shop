9ea896282254471b325ca814458aba730fa63c35
kenny <pletnev735@gmail.com>
2023-10-23 13:57:04 +0300

    add footer

--- modules/footer.php
+++ modules/footer.php
@@ -1,3 +1,62 @@
 <footer class="mt-5">
-    
+    <div class="container-user">
+        <section class="d-flex flex-column justify-content-between h-100">
+            <section class="d-flex flex-row justify-content-between align-items-start mt-5">
+                <div class="d-flex flex-row justify-content-between align-items-start header-footer">
+                    <div class="nav-link-font me-5">
+                        <h4>Помощь</h4>
+                        <ul>
+                            <li><a target="blank" href="#">Сообщество</a></li>
+                            <li><a target="blank" href="#">Правила сообщества</a></li>
+                            <li><a target="blank" href="#">Вопрос-ответ</a></li>
+                            <li><a target="blank" href="#">Как выбрать кофе?</a></li>
+                            <li><a target="blank" href="#">Контакты</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-link-font me-5">
+                        <h4>Компания</h4>
+                        <ul>
+                            <li><a target="blank" href="#">О компании</a></li>
+                            <li><a target="blank" href="#">Достижения</a></li>
+                            <li><a target="blank" href="#">Партнерская программа</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-link-font">
+                        <h4>Каталог</h4>
+                        <ul>
+                            <li><a target="blank" href="#">Кофе</a></li>
+                            <li><a target="blank" href="#">Архив сортов</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="d-flex flex-column align-items-center justify-content-end nav-link-font">
+                    <p style="font-size: 26px; font-weight: 700">8 (800) 333-49-80</p>
+                    <span style="font-size: 15px; font-weight: 400">Бесплатный звонок по России</span>
+                </div>
+            </section>
+            <section class="d-flex flex-row justify-content-end align-items-center mb-2">
+                    <div class="social-media d-flex flex-row align-items-center">
+                        <a target="blank" href="https://vk.com">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="14" viewBox="0 0 24 14" fill="none">
+                            <path d="M22.8472 0.94184C23.0078 0.399305 22.8472 0 22.0747 0H19.5182C18.8672 0 18.5677 0.342882 18.4071 0.724826C18.4071 0.724826 17.105 3.89757 15.2648 5.95486C14.6701 6.54948 14.3967 6.74045 14.0712 6.74045C13.9106 6.74045 13.6632 6.54948 13.6632 6.00694V0.94184C13.6632 0.290798 13.4809 0 12.9427 0H8.92361C8.51562 0 8.27257 0.303819 8.27257 0.585938C8.27257 1.20226 9.19271 1.34549 9.28819 3.0816V6.84896C9.28819 7.67361 9.14062 7.82552 8.8151 7.82552C7.94705 7.82552 5.83767 4.63976 4.58767 0.993924C4.33594 0.286458 4.08854 0 3.43316 0H0.876736C0.147569 0 0 0.342882 0 0.724826C0 1.40191 0.868056 4.76563 4.0408 9.21007C6.15451 12.2439 9.13194 13.8889 11.8403 13.8889C13.4679 13.8889 13.6675 13.5243 13.6675 12.895C13.6675 9.99566 13.52 9.72222 14.3359 9.72222C14.7135 9.72222 15.3646 9.91319 16.8837 11.3759C18.6198 13.112 18.9062 13.8889 19.8785 13.8889H22.4349C23.1641 13.8889 23.533 13.5243 23.3203 12.8038C22.8342 11.2891 19.5486 8.17274 19.401 7.96441C19.0234 7.4783 19.1319 7.26128 19.401 6.82726C19.4054 6.82292 22.526 2.43056 22.8472 0.94184Z" fill="white"/>
+                        </svg>
+                        </a>
+                        <a target="blank" href="https://youtube.com">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="17" viewBox="0 0 25 17" fill="none">
+                            <path d="M23.5885 2.60777C23.3159 1.58129 22.5127 0.772873 21.4928 0.498524C19.6443 0 12.2319 0 12.2319 0C12.2319 0 4.81961 0 2.97104 0.498524C1.9512 0.772917 1.14799 1.58129 0.875378 2.60777C0.380066 4.46832 0.380066 8.35017 0.380066 8.35017C0.380066 8.35017 0.380066 12.232 0.875378 14.0926C1.14799 15.1191 1.9512 15.8938 2.97104 16.1681C4.81961 16.6667 12.2319 16.6667 12.2319 16.6667C12.2319 16.6667 19.6443 16.6667 21.4928 16.1681C22.5127 15.8938 23.3159 15.1191 23.5885 14.0926C24.0838 12.232 24.0838 8.35017 24.0838 8.35017C24.0838 8.35017 24.0838 4.46832 23.5885 2.60777ZM9.80767 11.8746V4.82574L16.0029 8.35026L9.80767 11.8746Z" fill="white"/>
+                        </svg>
+                        </a>
+                        <a target="blank" href="https://web.telegram.org/k/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="19" viewBox="0 0 22 19" fill="none">
+                            <path d="M21.8945 1.66995L18.5937 17.2364C18.3447 18.335 17.6953 18.6084 16.7725 18.0908L11.7432 14.3848L9.3164 16.7188C9.04785 16.9873 8.82324 17.2119 8.30566 17.2119L8.66699 12.0899L17.9883 3.66702C18.3936 3.30569 17.9004 3.1055 17.3584 3.46683L5.83496 10.7227L0.87402 9.16995C-0.205082 8.83304 -0.224613 8.09085 1.09863 7.57327L20.5029 0.0976854C21.4014 -0.239229 22.1875 0.297881 21.8945 1.66995Z" fill="white"/>
+                        </svg>
+                        </a>
+                    </div>
+                    <div class="copyright-info nav-link-font d-flex flex-column justify-content-start align-items-end" style="font-size: 15px; font-weight: 400">
+                        <p>© 2023 "Ground Black"</p>
+                        <p>Все права защищены</p>
+                    </div>
+            </section>
+        </section>
+    </div>
 </footer>
